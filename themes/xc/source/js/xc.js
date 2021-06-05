ba439e380d64feb2b727a8b3421776dd10520769
wxc0914 <wxc0914@qq.com>
2021-06-05 18:05:09 +0800

    update the theme

--- themes/xc/source/js/xc.js
+++ themes/xc/source/js/xc.js
@@ -1,1 +1,7 @@
 InstantClick.init();
+
+$("figure.highlight").each(function(){
+    var lang = $(this).attr("class").split(" ")[1];
+    lang = lang.charAt(0).toUpperCase() + lang.substr(1).toLowerCase();
+    $(this).attr('lang', lang)
+})
