d1d4c729c93da80f240a03f9f291de9ed495183c
mac <mac@macdeMac-mini.local>
2021-07-11 16:04:34 +0800

    update the theme

--- themes/xc/source/js/xc.js
+++ themes/xc/source/js/xc.js
@@ -1,7 +1,12 @@
-InstantClick.init();
-
-$("figure.highlight").each(function(){
+// 给code块添加mac样式
+var codeblock = $("figure.highlight")
+codeblock.css("margin-top", "0");
+codeblock.each(function(){
     var lang = $(this).attr("class").split(" ")[1];
     lang = lang.charAt(0).toUpperCase() + lang.substr(1).toLowerCase();
-    $(this).attr('lang', lang)
+    $(this).before("<div class='code-mac-style' lang=" + lang + " ><div>");
 })
+// 去掉checklist的小圆点
+var checkboxs = $("input[type=checkbox]");
+checkboxs.parent().css("list-style", "none");
+checkboxs.parent().parent().css("padding-left", "0");
